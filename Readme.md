7e9e79f33ba6017941025e9f684396744252738a
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2022-04-21 19:13:44 -0300

    a

--- Readme.md
+++ Readme.md
@@ -3,12 +3,15 @@
 Projeto desenvolvido de software que le um arquivo com a matriz incidência dos vertices dos grafos. Com isso, fazer o aprimoramento dos conhecimentos sobre grafos e seus conceitos, utilizando 
 
 ## Perguntas para serem respondidas:
-[] O  grafo é simples? Por quê? Indique se há arestas múltiplas  ou laços. Indique, também, entre que vértices as arestas múltiplas ocorrem em que vértices ocorrem laços.
-  -> Quase feito, falta verificar se tem arestas múltiplas ou laços.
-[x] Qual sequência dos graus do grafo? Obs. Os graus devem estar em ordem não crescente.
-[x] Qual o número de arestas do grafo?
-[] O grafo é completo?
-[] O grafo é regular?
-[] O grafo é bipartido? Em caso afirmativo dê uma bipartição dos vértices do grafo.
-[] O grafo é bipartido completo? Em caso afirmativo dê uma bipartição dos vértices do grafo
+- [ ] O  grafo é simples? Por quê? Indique se há arestas múltiplas  ou laços. Indique, também, entre que vértices as arestas múltiplas ocorrem em que vértices ocorrem laços.
+  - [x] Descobrir se o grado e simples.
+  - [ ] Explicar o por que o grado e simples ou nao.
+  - [x] Verificar se tem arestas múltiplas.
+  - [x] Verificar se tem arestas laços.
+- [x] Qual sequência dos graus do grafo? Obs. Os graus devem estar em ordem não crescente.
+- [x] Qual o número de arestas do grafo?
+- [x] O grafo é completo?
+- [ ] O grafo é regular?
+- [ ] O grafo é bipartido? Em caso afirmativo dê uma bipartição dos vértices do grafo.
+- [ ] O grafo é bipartido completo? Em caso afirmativo dê uma bipartição dos vértices do grafo
 
--- grafo.py
+++ grafo.py
@@ -1,27 +1,58 @@
 arquivo = open('./A.txt', 'r')
 matriz = []
 sequency = []
-vertices = 0
+simples = 0
+laco = []
+multAresta = []
+n = 0
 
 for i in arquivo.readlines():
-    matriz.append(i.strip().split())
-
+	matriz.append(i.strip().split())
+matriz.pop()
 # print(matriz)
 
+# Verifica se o grafo e simples
 for linha in range(len(matriz)):
-    for coluna in range(len(matriz[linha])):
-        # print('%s' % matriz[linha][coluna], end=' ')
-        sequency.append(matriz[linha][coluna])
-        if(int(matriz[linha][coluna]) >= 1):
-            vertices += 1
-    # print()
+	for coluna in range(len(matriz[linha])):
+		# print('%s' % matriz[linha][coluna], end=' ')
+		sequency.append(matriz[linha][coluna])
+		if(int(matriz[linha][coluna]) == 1):
+			simples += 1
+			# print(f'Simples: {simples}')
+	# print()
+	n+=1
+	print(f'N: {n}')
 
-if(vertices == 11):
-    print('O grado e simples')
-    print(vertices)
+# Verifica se existe laco
+for linha in range(len(matriz)):
+	for coluna in range(len(matriz[linha])):
+		if((linha == coluna) and (int(matriz[linha][coluna]) >= 1)):
+			laco.append('No vertice v%d, existe um laco de grau %s' %(linha+1, matriz[linha][coluna]))
+		elif ((linha != coluna) and (int(matriz[linha][coluna]) > 1)):
+			multAresta.append('No vertice v%d, existe mais de uma aresta, assim possuindo grau %s' % (linha+1, matriz[linha][coluna]))
+
+complete = (n * (n-1))/2
+print(f'Complete: {n-1}')
+
+
+print(simples)
+if(simples == 11):
+	print('O grafo e simples e completo\n')
+	print(simples)
 else:
-    print('O grafo nao e simples')
-    print(vertices)
+	print('O grafo nao e simples e completo\n')
+	# print(simples)
+
+print('Vertices com laços:')
+for i in range(len(laco)):
+	print(laco[i])
+print()
+
+print('Vertices com arestas múltiplas:')
+for i in range(len(multAresta)):
+	print(multAresta[i])
+print()
+
 ordenada = sorted(sequency, reverse=True)
-print('Sequencia dos graus: { %s }' % ', '.join(ordenada))
+print('Sequencia dos graus: { %s }\n' % ', '.join(ordenada))
 print(f'Quantidade de arestas: {len(matriz)}')
